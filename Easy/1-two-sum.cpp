c1c1d00efc0c229861eee68b3ff2e44d34b5207f
Navya <navyanaivedya@gmail.com>
2026-03-19 15:44:36 +0530

    Added Two Sum solution code

--- Easy/1-two-sum.cpp
+++ Easy/1-two-sum.cpp
@@ -1,25 +1,23 @@
-//Hash Map used 
-//Time: O(n)
-//Space: O(n)
-
-#include <bits/stdc++.h>
+#include <vector>
 using namespace std;
 
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> mp;
+    vector<int> twoSum(vector<int>& nums, int target) { 
+        //nums is the list of numbers and target is the number we want the sum to become
 
+        // Loop through each number
         for (int i = 0; i < nums.size(); i++) {
-            int complement = target - nums[i];
 
-            if (mp.find(complement) != mp.end()) {
-                return {mp[complement], i};
+            // Check it with every number after it
+            for (int j = i + 1; j < nums.size(); j++) {
+
+                // If sum matches target, return indices
+                if (nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
             }
-
-            mp[nums[i]] = i;
         }
-
         return {};
     }
 };

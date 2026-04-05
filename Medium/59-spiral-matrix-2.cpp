b8ed8c841291a3a095f1c95e48128f2df2bde577
Navya <navyanaivedya@gmail.com>
2026-04-05 18:36:06 +0530

    Implemented spiral traversal using top, bottom, left, right boundaries

---
+++ Medium/59-spiral-matrix-2.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        vector<vector<int>> matrix(n, vector<int>(n, 0));
+        
+        int top = 0, bottom = n - 1;
+        int left = 0, right = n - 1;
+        
+        int num = 1;
+        
+        while (top <= bottom && left <= right) {
+            
+            // 1. Left -> Right
+            for (int i = left; i <= right; i++) {
+                matrix[top][i] = num++;
+            }
+            top++;
+            
+            // 2. Top -> Bottom
+            for (int i = top; i <= bottom; i++) {
+                matrix[i][right] = num++;
+            }
+            right--;
+            
+            // 3. Right -> Left
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    matrix[bottom][i] = num++;
+                }
+                bottom--;
+            }
+            
+            // 4. Bottom -> Top
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    matrix[i][left] = num++;
+                }
+                left++;
+            }
+        }
+        
+        return matrix;
+    }
+};
